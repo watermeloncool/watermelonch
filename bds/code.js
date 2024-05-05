eec553bf06b70b7320d12ea45541ee6c4875cae1
watermeloncool <149166592+watermeloncool@users.noreply.github.com>
2024-05-05 14:37:32 +0300

    Add files via upload

---
+++ bds/code.js
@@ -0,0 +1,13 @@
+
+/*
+let video = document.getElementById('video-background');
+
+video.play();
+
+  video.addEventListener('ended', function() {
+    video = document.getElementById('video-background');
+    console.log('fin vidéo');
+    video.currentTime = 0;
+    video.play();
+  });
+  */
