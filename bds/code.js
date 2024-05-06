e68c536f23f085e1a3987067eda26a58a54a2b35
watermeloncool <149166592+watermeloncool@users.noreply.github.com>
2024-05-07 01:13:01 +0300

    Add files via upload

--- bds/code.js
+++ bds/code.js
@@ -11,3 +11,20 @@ video.play();
     video.play();
   });
   */
+
+
+const menubtn = document.getElementById('menumobileouvrir');
+const fermerbtn = document.getElementById('menumobilefermer');
+const sousmenumobile = document.getElementById('sousmenumobile');
+
+menubtn.addEventListener('click', function() {
+  sousmenumobile.style.display = 'block';
+  menubtn.style.display = 'none';
+  fermerbtn.style.display = 'inline-block';
+});
+
+fermerbtn.addEventListener('click', function() {
+  sousmenumobile.style.display = 'none';
+  menubtn.style.display = 'inline-block';
+  fermerbtn.style.display = 'none';
+});
